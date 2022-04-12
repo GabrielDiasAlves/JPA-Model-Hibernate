065ebd7c3ad11e650a8526b5ec38fb26528c4f01
Gabriel <gabrieldalves@outlook.com.br>
2022-04-11 21:13:44 -0300

    Embeddable

--- src/main/java/teste/consulta/ObterMediaFilmes.java
+++ src/main/java/teste/consulta/ObterMediaFilmes.java
@@ -6,6 +6,7 @@ import modelo.consulta.NotaFilme;
 public class ObterMediaFilmes {
 
 	public static void main(String[] args) {
+		
 		DAO<NotaFilme> dao = new DAO<>(NotaFilme.class);
 		
 		NotaFilme nota = dao.consultarUm("obterMediaGeralDosFilmes"); 
@@ -14,5 +15,4 @@ public class ObterMediaFilmes {
 		
 		dao.fechar();
 	}
-
 }
